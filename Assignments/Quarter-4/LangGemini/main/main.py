4a4850a9a878d5a6fb54271363cb2970183d76a9
Abrar-Kamoka <abrarabbas.ak@gmail.com>
2025-03-09 17:32:58 +0500

    langchain, gemini - chat History

--- Assignments/Quarter-4/LangGemini/main/main.py
+++ Assignments/Quarter-4/LangGemini/main/main.py
@@ -1,58 +1,85 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import (
+                                    ChatPromptTemplate, 
+                                    SystemMessagePromptTemplate, 
+                                    HumanMessagePromptTemplate, 
+                                    MessagesPlaceholder,
+                                    )
+from langchain.chains import ConversationChain, LLMChain
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model = "gemini-1.5-pro", google_api_key = os.getenv("GOOGLE_API_KEY"))
 
-prompt = PromptTemplate(
-    template=(
-        "You are a helpful assistant. Below is the conversation history:\n"
-        "{conversation}\n"
-        "Based on this, respond to the user's latest messages:\n"
-        "{latest_message}"
-    ), 
-    input_variables=["conversation", "latest_message"]
-)
+memory = ConversationSummaryBufferMemory(llm = llm, max_token_limit=1000)   # Optional: k, memory_key, human_prefix, ai_prefix
 
-chain = prompt | llm
-
-conversation = [
-    SystemMessage(content="You are a helpful assistant."),
-]
+chain = ConversationChain(llm=llm, memory=memory)
 
 while True:
-    for message in conversation:
-        if isinstance(message, HumanMessage):
-            print(f"User: {message.content}")
-        elif isinstance(message, AIMessage):
-            print(f"AI: {message.content}")
-        elif isinstance(message, SystemMessage):
-            print(f"System: {message.content}")
-    
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    response = chain.invoke(user_input)
+    print("\nAI: ", response)
 
-    human_message = input("\nWrite your message: ")
 
-    conversation.append(HumanMessage(content=human_message))
+## before memory usage
+                    # prompt = ChatPromptTemplate.from_messages(
+                    #     [
+                    #         SystemMessagePromptTemplate.from_template("You are a helpful assistant. Respond averagely and accurately."),
+                    #         HumanMessagePromptTemplate.from_template("{conversation}"),
+                    #         HumanMessagePromptTemplate.from_template("{latest_message}"),    
+                    #     ]
 
-    formatted_conversation = "\n".join(
-        f"User: {msg.content}" if isinstance(msg, HumanMessage)
-        else f"AI: {msg.content}" if isinstance(msg, AIMessage)
-        else f"System: {msg.content}"
-        for msg in conversation
-    )
+                    #     # template=(  
+                    #     #     "You are a helpful assistant. Below is the conversation history:\n"
+                    #     #     "{conversation}\n"
+                    #     #     "Based on this, respond to the user's latest messages:\n"
+                    #     #     "{latest_message}"
+                    #     # ), 
+                    #     # input_variables=["conversation", "latest_message"]
+                    # )
 
-    AI_response = chain.invoke({
-        "conversation": formatted_conversation,
-        "latest_message": human_message
-    })
+                    # chain = prompt | llm
 
-    conversation.append(AIMessage(content=AI_response.content))
+                    # conversation = [
+                    #     SystemMessage(content="\nYou are a helpful assistant. Respond averagely and accurately."),
+                    # ]
 
-    print(f"\nAI: {AI_response.content}")
+                    # while True:
+                    #     for message in conversation:
+                    #         if isinstance(message, HumanMessage):
+                    #             print(f"User: {message.content}")
+                    #         elif isinstance(message, AIMessage):
+                    #             print(f"AI: {message.content}")
+                    #         elif isinstance(message, SystemMessage):
+                    #             print(f"System: {message.content}")
+                        
 
+                    #     human_message = input("\nWrite your message: ")
+
+                    #     conversation.append(HumanMessage(content=human_message))
+
+                    #     formatted_conversation = "\n".join(
+                    #         f"User: {msg.content}" if isinstance(msg, HumanMessage)
+                    #         else f"AI: {msg.content}" if isinstance(msg, AIMessage)
+                    #         else f"System: {msg.content}"
+                    #         for msg in conversation
+                    #     )
+
+                    #     AI_response = chain.invoke({
+                    #         "conversation": formatted_conversation,
+                    #         "latest_message": human_message
+                    #     })
+
+                    #     conversation.append(AIMessage(content=AI_response.content))
+
+                    #     print(f"\nAI: {AI_response.content}")
+## end - before memory usage
 
 
 
